16f0cdf4ae9f0abae7940267c199b28fbad1eb64
Nils Droste droste <droste>
2018-02-06 17:52:33 +0100

    update

--- analysis.R
+++ analysis.R
@@ -8,6 +8,10 @@
 # loading packages
 library(qmethod)
 
-# reading data
-raw.data <- read.csv(paste(getwd(),"/data/TESTING_A_QMethod_comparative_study_on_Circular_Green_and_BioEconomy_approaches_1_0_TESTING.csv",sep = ""), header = T, sep = ';')
+# reading data #might need some specifications because we have had changes in the questionnaire while saving all to the same file
+raw.data <- read.csv(paste(getwd(),"/data/TESTING_A_QMethod_comparative_study_on_Circular_Green_and_BioEconomy_approaches_1_0_TESTING.csv",sep = ""), header = T, sep = ';', nrows=13)
+
+# transposing the dataframe to required format for qmethod
+qsorts <- as.data.frame(t(raw.data[,3:38]))
+colnames(qsorts) <- raw.data$sid
 
