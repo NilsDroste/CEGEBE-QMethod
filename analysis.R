e3c6748bd6c1669e1337b746a47d7e3a30006b61
Nils Droste droste <droste>
2018-02-06 18:05:52 +0100

    update

--- analysis.R
+++ analysis.R
@@ -11,7 +11,21 @@ library(qmethod)
 # reading data #might need some specifications because we have had changes in the questionnaire while saving all to the same file
 raw.data <- read.csv(paste(getwd(),"/data/TESTING_A_QMethod_comparative_study_on_Circular_Green_and_BioEconomy_approaches_1_0_TESTING.csv",sep = ""), header = T, sep = ';', nrows=13)
 
-# transposing the dataframe to required format for qmethod
-qsorts <- as.data.frame(t(raw.data[,3:38]))
-colnames(qsorts) <- raw.data$sid
+# check for duplicates
+duplicated(raw.data[,3:38])
 
+# transposing the dataframe to required format for qmethod #note that there still is some interesting data stored in raw.data
+qsorts <- as.data.frame(t(raw.data[!duplicated(raw.data[,3:38]),3:38]))
+colnames(qsorts) <- raw.data[!duplicated(raw.data[,3:38]),]$sid
+
+
+# 1 analysis ----
+
+# setting the variables
+factors <- 3 # The number of factors to extract and rotate # TODO: understand what this is
+
+# run the analysis
+results <-  qmethod(qsorts, 
+                    nfactors = factors,
+                    rotation = "varimax", # Can be replaced by "none"
+                    forced = TRUE)
